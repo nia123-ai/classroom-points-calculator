ddaefdbd4505fe252e795b8d4d8c9495759c0b0f
nia123-ai <nia.padh@gmail.com>
2026-08-12 14:49:37 -0400

    test

--- identation.py
+++ identation.py
@@ -26,3 +26,27 @@ if name == "Nia":
     print("Hello, Nia!")
 else:
     print("Hello, stranger!")
+
+has_puddles = input("Are there puddles on the ground? (yes/no):")
+if has_puddles == "yes":
+    shoes="boots"
+    print("Wear boots.", shoes)
+else:
+    shoes="sneakers"
+    print("Wear sneakers.", shoes)
+pizza = int(input("How many slices of pizza do you want? "))
+if pizza > 3:
+    print("That's a lot of pizza!")
+else:
+    print("Enjoy your pizza!")
+mom_saw = input("Did mom see you eating the pizza? (yes/no): ")
+if mom_saw.lower() == "yes":
+    print("Run mom knows🏃‍♂️")
+else:
+    print("Enjoy your pizza without getting caught!🍕")
+sleep_hours= int(input("How many hours did you sleep last night? "))
+
+if sleep_hours >= 8:
+    print("You had a good night's sleep!😴")
+else:
+    print("You should get more sleep!😴")
